6cd7f56889a2da52473eabd6418ea0bef0471b9a
gen-te <gen1311135@gmail.com>
2022-03-18 00:11:33 +0900

    ~4-3 end

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -22,9 +22,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "task #{task.name} created"
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_url, notice: "task #{@task.name} created"
+    else
+      render :new
+    end
   end
   
   def destroy
--- taskleaf/db/schema.rb
+++ taskleaf/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_13_042237) do
+ActiveRecord::Schema.define(version: 2022_03_17_030700) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
 end
