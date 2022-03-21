e5db452b7ca6f3c9e52f2ae949fd09a13875cd3d
gen-te <gen1311135@gmail.com>
2022-03-21 23:17:26 +0900

    ~4-5-4 admin user function end

--- taskleaf/app/controllers/admin/users_controller.rb
+++ taskleaf/app/controllers/admin/users_controller.rb
@@ -1,26 +1,49 @@
 class Admin::UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_user_url(@user), notice: 'user #{@user.name} created'
+      redirect_to admin_user_url(@user), notice: "user #{@user.name} created"
     else
       render :new
     end
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
-  def index
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "user #{@user.name} created"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, norice: "user #{@user.name} destroy"
   end
 
   private
