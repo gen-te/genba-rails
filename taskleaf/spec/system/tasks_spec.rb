e0f4092f94f2d357b9424bbb1fbb729c8f3b8456
gen-te <gen1311135@gmail.com>
2022-03-27 14:25:41 +0900

    ~5.8

---
+++ taskleaf/spec/system/tasks_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+    describe '一覧表示機能' do
+        before do
+            # user A 作成
+            user_a = FactoryBot.create(:user, name: 'UserA', email: 'a@example.com')
+            # user A のタスク作成
+            FactoryBot.create(:task, name: 'first task', user: user_a)
+        end
+        context "User A ログインしている時" do
+            before do
+                # user A login
+                visit login_path
+                fill_in 'email', with: 'a@example.com'
+                fill_in 'password', with: 'password'
+                click_button 'ログイン'
+            end
+
+            it 'User Aが作成したタスクが表示される' do
+                # 作成すみのタスクの名称が画面上に表示されていることを確認
+                expect(page).to have_content 'first task'
+            end
+        end
+    end
+end
