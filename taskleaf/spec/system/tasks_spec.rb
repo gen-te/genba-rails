d7bf8947533e558fa3224c14ea34ad4c4e81be78
gen-te <gen1311135@gmail.com>
2022-03-27 15:52:10 +0900

    5.11

--- taskleaf/spec/system/tasks_spec.rb
+++ taskleaf/spec/system/tasks_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
-    describe '一覧表示機能' do
-        let(:user_a){FactoryBot.create(:user, name:'UserA', email: 'a@example.com')}
-        let(:user_b){FactoryBot.create(:user, name:'UserB', email: 'b@example.com')}
-        before do
-            # user A のタスク作成
-            FactoryBot.create(:task, name: 'first task', user: user_a)
+    let(:user_a){FactoryBot.create(:user, name:'UserA', email: 'a@example.com')}
+    let(:user_b){FactoryBot.create(:user, name:'UserB', email: 'b@example.com')}
+    let!(:task_a){FactoryBot.create(:task, name:'first task', user: user_a)}
 
-            # user A login
-            visit login_path
-            fill_in 'email', with: login_user.email
-            fill_in 'password', with: login_user.password
-            click_button 'ログイン'
-            
-        end
+    before do            # user A login
+        visit login_path
+        fill_in 'email', with: login_user.email
+        fill_in 'password', with: login_user.password
+        click_button 'ログイン'
+    end
+    
+    describe '一覧表示機能' do
         context 'User A ログインしている時' do
             let(:login_user){user_a}
 
@@ -32,6 +30,19 @@ describe 'タスク管理機能', type: :system do
                 expect(page).to have_no_content 'first task' 
             end
         end
+    end
 
+    describe '詳細表示機能' do
+        context 'User Aがログインしている時' do
+            let(:login_user){user_a}
+
+            before do
+                visit task_path(task_a)
+            end
+
+            it 'User Aが作成したタスクが表示される' do
+                expect(page).to have_content 'first task'
+            end
+        end
     end
 end
